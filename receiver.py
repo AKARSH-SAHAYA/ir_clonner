fb129012853e3d0e3acdac3b9b455ee187b1e205
AKARSH <akarshsahaya@gmail.com>
2025-06-29 20:50:38 +0530

    added the time_stamp difference code

--- receiver.py
+++ receiver.py
@@ -7,15 +7,15 @@ time_stamp=[]
 def counter(Pin):
   time_stamp.append(time.ticks_us())
   
-ir= Pin(18,Pin.IN)
-ir.irq(trigger= Pin.IRQ_FALLING | Pin.IRQ_RISING,handler= counter())
+ir = Pin(18,Pin.IN)
+ir.irq(trigger= Pin.IRQ_FALLING | Pin.IRQ_RISING,handler= counter)
 
 
 while True:
     if time_stamp:
        
         for i in range(len(time_stamp)-1):
-           ir_signal.append(time_stamp[i]-time_stamp[i+1])
+           ir_signal.append(time_stamp[i+1]-time_stamp[i])
        
         print("Captured IR timings (in microseconds):", ir_signal)
         ir_signal.clear()
--- time_stamp_difference.py
+++ time_stamp_difference.py
@@ -3,6 +3,6 @@ import math
 ir_signal =[ ]
 ls=[2,5,9,12,18,25,35]
 for i in range(len(ls)-1):
-    ir_signal.append(abs(ls[i]-ls[i+1]))
+    ir_signal.append(ls[i+1]-ls[i])
 
 print(ir_signal)    
